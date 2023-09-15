2aa23930745f772e6365811357920af884e03a47
Erekhinsky_Andrey <dron12301230@yandex.ru>
2023-09-15 22:55:15 +0300

    more

--- src/main/java/pages/VacanciesPage.java
+++ src/main/java/pages/VacanciesPage.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.PropsHandler;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VacanciesPage extends Page {
 
@@ -15,10 +18,53 @@ public class VacanciesPage extends Page {
     WebElement jobFormInput;
 
     @FindBy(xpath = "//div[@class=\"vacancy-card__title\"]/a")
-    WebElement jobsName;
+    List<WebElement> jobsNames;
 
-    @FindBy(xpath = "//div[@data-tab=\"dev\"]//span[contains(text(), \"Больше вакансий\")]/../..")
-    WebElement moreVacanciesButton;
+    @FindBy(xpath = "//div[@class=\"basic-salary\"]")
+    List<WebElement> salaries;
+
+    @FindBy(xpath = "//option[@value=\"salary_desc\"]")
+    WebElement salariesSortDesc;
+
+    @FindBy(xpath = "//select[@class=\"select__input\"]/option[@value=\"relevance\"]/..")
+    WebElement salariesSort;
+
+    @FindBy(xpath = "//input[@placeholder=\"От\"]")
+    WebElement salariesFilterInput;
+
+    @FindBy(xpath = "//select[@class=\"select__input\"]/option[@value=\"1\"]/..")
+    WebElement qualSelect;
+
+    @FindBy(xpath = "//select[@class=\"select__input\"]/option[@value=\"1\"]")
+    WebElement internQualSelect;
+
+    @FindBy(xpath = "//select[@class=\"select__input\"]/option[@value=\"3\"]")
+    WebElement juniorQualSelect;
+
+    @FindBy(xpath = "//select[@class=\"select__input\"]/option[@value=\"4\"]")
+    WebElement middleQualSelect;
+
+    @FindBy(xpath = "//select[@class=\"select__input\"]/option[@value=\"5\"]")
+    WebElement seniorQualSelect;
+
+    @FindBy(xpath = "//select[@class=\"select__input\"]/option[@value=\"6\"]")
+    WebElement leadQualSelect;
+
+    @FindBy(xpath = "//div[@class=\"vacancy-card__skills\"]/span/span/span/span")
+    List<WebElement> qualAndSpec;
+
+    @FindBy(xpath = "//div[@class=\"specs-picker__specs\"]")
+    WebElement specFilterOpen;
+
+    @FindBy(xpath = "//input[@placeholder=\"Поиск\"]/../../span[@class=\"text-input specs-search__input " +
+            "text-input--appearance-search\"]/input")
+    WebElement specFilterSearch;
+
+    @FindBy(xpath = "//div[@data-dropdown-open=\"open\"]")
+    WebElement specFilterAfterSearch;
+
+    @FindBy(xpath = "//button[@class=\"button-comp button-comp--appearance-primary button-comp--size-l\"]")
+    WebElement specFilterSubmitButton;
 
     public VacanciesPage(WebDriver driver) {
         super(driver);
@@ -32,9 +78,119 @@ public class VacanciesPage extends Page {
         wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe(url)));
     }
 
+    public void specSelectOpen(String input) {
+        String url = driver.getCurrentUrl();
+        specFilterOpen.click();
+        specFilterSearch.sendKeys(input);
+        specFilterAfterSearch.click();
+        specFilterSubmitButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe(url)));
+    }
 
+    public void qualSelect(String select) {
+        String url = driver.getCurrentUrl();
+        qualSelect.click();
+        if (select.equals(PropsHandler.get("qualIntern"))) {
+            internQualSelect.click();
+        } else if (select.equals(PropsHandler.get("qualJun"))) {
+            juniorQualSelect.click();
+        } else if (select.equals(PropsHandler.get("qualMid"))) {
+            middleQualSelect.click();
+        }  else if (select.equals(PropsHandler.get("qualSen"))) {
+            seniorQualSelect.click();
+        } else if (select.equals(PropsHandler.get("qualLead"))) {
+            leadQualSelect.click();
+        }
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe(url)));
+    }
 
+    public List<String> getJobNames() {
+        List<String> strJobNames = new ArrayList<>();
+        for (WebElement element : jobsNames) {
+            strJobNames.add(element.getText().toLowerCase());
+        }
+        return strJobNames;
+    }
 
+    public void searchSalary(String input) {
+        String url = driver.getCurrentUrl();
+        salariesFilterInput.clear();
+        salariesFilterInput.sendKeys(input);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe(url)));
+    }
+
+    public void salarySortDesc() {
+        String url = driver.getCurrentUrl();
+        salariesSort.click();
+        salariesSortDesc.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe(url)));
+    }
+
+    public List<String> getQual() {
+        List<String> qual = new ArrayList<>();
+        for (WebElement element : qualAndSpec) {
+            String str = element.getText();
+            if (!str.endsWith(",")) {
+                qual.add(str.substring(0, str.length() - 1));
+            }
+        }
+        return qual;
+    }
+
+    public List<String> getSpec() {
+        List<String> spec = new ArrayList<>();
+        for (WebElement element : qualAndSpec) {
+            String str = element.getText();
+            if (str.endsWith(",")) {
+                spec.add(str);
+            }
+        }
+        return spec;
+    }
+
+    public List<Double> getSalaryValue() {
+        List<Double> numDoubles = new ArrayList<>();
+        for (WebElement element : salaries) {
+            List<String> strs = List.of(element.getText().split(" "));
+            List<String> numbers = new ArrayList<>();
+            boolean curNum = false;
+            for (String str : strs) {
+                if (str.matches("\\d+")) {
+                    if (curNum) {
+                        String prev = numbers.get(numbers.size() - 1);
+                        numbers.remove(numbers.size() - 1);
+                        numbers.add(prev + str);
+                    } else {
+                        curNum = true;
+                        numbers.add(str);
+                    }
+                } else {
+                    curNum = false;
+                }
+                switch (str) {
+                    case "$" -> numDoubles.add(Double.parseDouble(numbers.get(numbers.size() - 1)) *
+                            Double.parseDouble(PropsHandler.get("dollar_rate")));
+                    case "€" -> numDoubles.add(Double.parseDouble(numbers.get(numbers.size() - 1)) *
+                            Double.parseDouble(PropsHandler.get("euro_rate")));
+                    case "₽" -> numDoubles.add(Double.parseDouble(numbers.get(numbers.size() - 1)));
+                }
+            }
+        }
+        return numDoubles;
+    }
+
+    public boolean isSorted(List<Double> a) {
+        for (int i = 0; i < a.size() - 1; i++) {
+            if (a.get(i) < a.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 }
--- src/test/java/VacanciesPageTest.java
+++ src/test/java/VacanciesPageTest.java
@@ -1,21 +1,15 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import pages.MainPage;
 import pages.VacanciesPage;
 import utils.DriverHandler;
 import utils.PropsHandler;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class VacanciesPageTest {
-
-    @FindBy(xpath = "//div[@class=\"vacancy-card__title\"]/a")
-    List<WebElement> jobsName;
 
     @BeforeAll
     public static void prepareDrivers() {
@@ -30,15 +24,90 @@ public class VacanciesPageTest {
             VacanciesPage vacanciesPage = new VacanciesPage(driver);
             vacanciesPage.loadSite(PropsHandler.get("url_vacancies"));
 
-            String newUrl = PropsHandler.get("url_vacancies_keyword") + PropsHandler.get("search_key_word")
-                    + PropsHandler.get("url_jobs_keyword_tail");
             vacanciesPage.searchJob(PropsHandler.get("search_key_word"));
+            List<String> jobNames = vacanciesPage.getJobNames();
 
-            assertEquals(driver.getCurrentUrl(), newUrl);
-
-            for (jobsName.listIterator())
+            for (String element : jobNames) {
+                assertTrue(element.contains(PropsHandler.get("search_key_word")));
+            }
 
             driver.quit();
         });
     }
+
+    @Test
+    void testSalaryFilter() {
+        List<WebDriver> drivers = DriverHandler.getDrivers();
+
+        drivers.parallelStream().forEach(driver -> {
+            VacanciesPage vacanciesPage = new VacanciesPage(driver);
+            vacanciesPage.loadSite(PropsHandler.get("url_vacancies"));
+
+            vacanciesPage.searchSalary(PropsHandler.get("salary_filter"));
+            List<Double> salaryValue = vacanciesPage.getSalaryValue();
+
+            for (Double element : salaryValue) {
+                assertTrue(element >= Double.parseDouble(PropsHandler.get("salary_filter")));
+            }
+
+            driver.quit();
+        });
+    }
+
+    @Test
+    void testQualFilter() {
+        List<WebDriver> drivers = DriverHandler.getDrivers();
+
+        drivers.parallelStream().forEach(driver -> {
+            VacanciesPage vacanciesPage = new VacanciesPage(driver);
+            vacanciesPage.loadSite(PropsHandler.get("url_vacancies"));
+
+            vacanciesPage.qualSelect(PropsHandler.get("qualMid"));
+            List<String> qualList = vacanciesPage.getQual();
+
+            for (String element : qualList) {
+                assertTrue(element.contains(PropsHandler.get("qualMid")));
+            }
+
+            driver.quit();
+        });
+    }
+
+    @Test
+    void testSpecFilter() {
+        List<WebDriver> drivers = DriverHandler.getDrivers();
+
+        drivers.parallelStream().forEach(driver -> {
+            VacanciesPage vacanciesPage = new VacanciesPage(driver);
+            vacanciesPage.loadSite(PropsHandler.get("url_vacancies"));
+
+            vacanciesPage.specSelectOpen(PropsHandler.get("search_spec_key_word_eng"));
+            List<String> specList = vacanciesPage.getSpec();
+
+            for (String element : specList) {
+                System.out.println(element + " " + PropsHandler.get("search_spec_key_word_rus") + "\n");
+                assertTrue(element.contains(PropsHandler.get("search_spec_key_word_rus")));
+            }
+
+            driver.quit();
+        });
+    }
+
+    @Test
+    void testSalarySort() {
+        List<WebDriver> drivers = DriverHandler.getDrivers();
+
+        drivers.parallelStream().forEach(driver -> {
+            VacanciesPage vacanciesPage = new VacanciesPage(driver);
+            vacanciesPage.loadSite(PropsHandler.get("url_vacancies"));
+
+            vacanciesPage.salarySortDesc();
+            List<Double> salaryArray = vacanciesPage.getSalaryValue();
+
+            assertTrue(vacanciesPage.isSorted(salaryArray));
+
+            driver.quit();
+        });
+    }
+
 }
